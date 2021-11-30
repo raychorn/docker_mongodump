c742480da3d2d7262ecc49d8241383c01ae932dd
Ray C Horn <raychorn@gmail.com>
2021-11-30 14:54:54 -0700

    updates.

---
+++ entrypoint.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+ENVPATH=$ROOT/.env
+echo "Environment file: $ENVPATH"
+
+if [ -f "$ENVPATH" ]; then
+    echo "Importing environment variables."
+    export $(cat $ENVPATH | sed 's/#.*//g' | xargs)
+    echo "Done importing environment variables."
+else
+    echo "ERROR: Environment variables not found. Please run the following command to generate them:"
+    sleep infinity
+fi
+
+if [ ! -d "$DEST" ]; then
+    mkdir -p $DEST
+fi
+
+PY=$(which python3.9)
+
+if [ ! -f "$PY" ]; then
+    echo "ERROR: Python 3.9 not found. Please install it."
+    sleep infinity
+fi
+
+echo "PY:$PY"
+
+PIP3=$(which pip3.9)
+
+if [ ! -f "$PIP3" ]; then
+    echo "ERROR: Pip 3.9 not found. Please install it."
+    sleep infinity
+fi
+
+echo "PIP3:$PIP3"
+
+VIRTUALENV=$(which virtualenv)
+
+if [ ! -f "$VIRTUALENV" ]; then
+    echo "ERROR: virtualenv not found. Please install it."
+    sleep infinity
+fi
+
+echo "VIRTUALENV:$VIRTUALENV"
+
+DIRNAME=$(date +%m-%d-%y-%H-%M-%S)
+DESTDIR=$DEST/$DIRNAME
+mkdir -p $DESTDIR
+# mongodb://root:sisko%407660%24boo@mongodb1-10.web-service.org:27017,mongodb2-10.web-service.org:27017,mongodb3-10.web-service.org:27017/?replicaSet=rs0&authSource=admin
+#mongodump -h <your_database_host> -d <your_database_name> -u $USERNAME -p $PASSWORD -o $DEST
